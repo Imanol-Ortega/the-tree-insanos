20403c0b143d4dffb6b1763f62e3ec37a6acb024
Imanol-Ortega <59693036+Imanol-Ortega@users.noreply.github.com>
2023-06-04 18:57:58 -0400

    cambios

--- huespedes.py
+++ huespedes.py
@@ -1,22 +1,22 @@
-import tkinter
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import customtkinter
 import mysql.connector
 
+
 def retornar_Menu(huespedes, menu):
     huespedes.withdraw()
     menu.deiconify()
 
+
 def ventana_huespedes(menu):
     root_tk = tk.Toplevel()
-    root_tk.geometry("1200x500")
+    root_tk.geometry("1200x600")
     root_tk.title("Huespedes")
     root_tk.config(bg="#000")
-    datos = {}
-    seleccion = 0
-    descuento = 0
+    root_tk.resizable(0, 0)
+    root_tk.protocol("WM_DELETE_WINDOW", root_tk)
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -31,21 +31,20 @@ def ventana_huespedes(menu):
         tecla = event.char
         tecla_presionada = cajaHabitacion.get()
         tecla_presionada = tecla_presionada + tecla
-        print("tecla: ", tecla_presionada)
         cargar_tabla1(tecla_presionada,
                       "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
 
     def guardar():
-        global descuento
+        descuento = 0
         if cajaHabitacion.get() != '' and item_selected2(True) != 0 and cajaDias.get() != '' and radio_var.get() != 0:
-            Nuero = int(cajaHabitacion.get())
+            Numero = int(cajaHabitacion.get())
             tipo = item_selected2(True)
             dias = int(cajaDias.get())
             tipoPago = int(radio_var.get())
             mycursor.execute("SELECT id FROM cliente")
             id = mycursor.fetchall()
             for ids in id:
-                if Nuero in ids:
+                if Numero in ids:
                     mycursor.execute("UPDATE cliente SET Estado =%s WHERE id = %s",
                                      (2, ids[0]))
                     mydb.commit()
@@ -56,15 +55,13 @@ def ventana_huespedes(menu):
                 descuento = 0.1
             if dias > 10:
                 descuento = descuento + 0.02
+            print(tipo[0])
             subtotal = dias*int(tipo[1])
             total = subtotal - (subtotal*descuento)
             mycursor.execute("INSERT INTO cliente (NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total,Estado) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
-                             (Nuero, tipo[0], tipo[1], dias, subtotal, descuento*100, total, 1))
+                             (Numero, tipo[0], tipo[1], dias, subtotal, descuento*100, total, 1))
             mydb.commit()
-            for i in tree.get_children():
-                tree.delete(i)
-            cargar_tabla1(
-                '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
+            limpiar()
 
     def limpiar():
         cajaDias.delete(0, tk.END)
@@ -73,27 +70,38 @@ def ventana_huespedes(menu):
         contado._variable.set(0)
         tree2.selection_remove(tree2.focus())
         tree.selection_remove(tree.focus())
+        for i in tree.get_children():
+            tree.delete(i)
+        cargar_tabla1(
+            '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
 
     def borrar():
-        seleccionado = item_selected1(True)
-        if seleccionado:
-            mycursor.execute("DELETE FROM cliente WHERE id=%s ", [
-                             seleccionado[0]])
-            mydb.commit()
-            for i in tree.get_children():
-                tree.delete(i)
-            cargar_tabla1(
-                '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
+        try:
+            if item_selected1(True)[0]:
+                mycursor.execute("DELETE FROM cliente WHERE id=%s ", [
+                    item_selected1(True)[0]])
+                mydb.commit()
+                for i in tree.get_children():
+                    tree.delete(i)
+                cargar_tabla1(
+                    '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
+        except TypeError:
+            messagebox.showerror(
+                "ERROR", "Porfavor seleccione el elemento a borrar")
 
     def modificar():
-        if seleccion:
-            mycursor.execute("UPDATE cliente SET Dias = %s WHERE id = %s",
-                             (int(cajaDias.get()), int(seleccion)))
-            mydb.commit()
-            for i in tree.get_children():
-                tree.delete(i)
-            cargar_tabla1(
-                '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
+        try:
+            if item_selected1(True)[0]:
+                mycursor.execute("UPDATE cliente SET Dias = %s WHERE id = %s",
+                                 (int(cajaDias.get()), item_selected1(True)[0]))
+                mydb.commit()
+                for i in tree.get_children():
+                    tree.delete(i)
+                cargar_tabla1(
+                    '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
+        except TypeError:
+            messagebox.showerror(
+                "ERROR", "Porfavor seleccione el elemento a modificar")
 
     def cargar_tabla1(tecla, sql):
         mycursor.execute(sql+"%s", ['%'+tecla+'%'])
@@ -110,17 +118,15 @@ def ventana_huespedes(menu):
                 f[1], f[2]))
 
     def item_selected1(event):
-        global seleccion
         record = 0
-        cajaDias.delete(0, tk.END)
+        # cajaDias.delete(0, tk.END)
         cajaHabitacion.delete(0, tk.END)
         for selected_item in tree.selection():
             item = tree.item(selected_item)
             record = item['values']
             cajaHabitacion.insert(0, record[1])
-            cajaDias.insert(0, record[4])
-            seleccion = record[0]
-        return record
+            # cajaDias.insert(0, record[4])
+            return record
 
     def item_selected2(event):
         record = 0
@@ -163,35 +169,40 @@ def ventana_huespedes(menu):
     buttonBorrar.grid(column=0, row=7, padx=5, pady=5)
     buttonModificar.grid(column=1, row=7, padx=5, pady=5)
     buttonSalir.grid(column=1, row=8, padx=5, pady=5)
-    columns1 = ('Id', 'Nuero', 'Tipo', 'Costo', 'Dias',
+
+    # TABLA PARA MOSTRAR RESERVACION
+    columns1 = ('Id', 'Numero', 'Tipo', 'Costo', 'Dias',
                 'SubTotal', 'Descuento', 'Total')
     tree = ttk.Treeview(root_tk, columns=columns1, show='headings')
     tree.heading('Id', text='Id')
-    tree.heading('Nuero', text='Nuero')
+    tree.heading('Numero', text='Numero')
     tree.heading('Tipo', text='Tipo')
     tree.heading('Costo', text='Precio')
     tree.heading('Dias', text='Dias')
     tree.heading('SubTotal', text='SubTotal')
     tree.heading('Descuento', text='%Descuento')
     tree.heading('Total', text='Total')
-    tree.column('Id', width=100, anchor='center')
-    tree.column('Nuero', width=100, anchor='center')
-    tree.column('Tipo', width=100, anchor='center')
-    tree.column('Costo', width=100, anchor='center')
-    tree.column('Dias', width=100, anchor='center')
-    tree.column('SubTotal', width=100, anchor='center')
-    tree.column('Descuento', width=100, anchor='center')
-    tree.column('Total', width=100, anchor='center')
+    tree.column('Id', width=20, anchor='w')
+    tree.column('Numero', width=60, anchor='w')
+    tree.column('Tipo', width=100, anchor='w')
+    tree.column('Costo', width=100, anchor='w')
+    tree.column('Dias', width=40, anchor='w')
+    tree.column('SubTotal', width=100, anchor='w')
+    tree.column('Descuento', width=80, anchor='w')
+    tree.column('Total', width=100, anchor='w')
     tree.grid(row=0, column=3, pady=40)
     tree.bind('<<TreeviewSelect>>', item_selected1)
+
+    # TABLA PARA MOSTRAR TIPO HABITACION
     columns2 = ('Descripcion', 'Costo')
     tree2 = ttk.Treeview(root_tk, columns=columns2, show='headings')
     tree2.heading('Descripcion', text='Tipo Habitacion')
     tree2.heading('Costo', text='Precio')
-    tree2.column('Descripcion', width=120, anchor='center')
-    tree2.column('Costo', width=100, anchor='center')
+    tree2.column('Descripcion', width=120, anchor='w')
+    tree2.column('Costo', width=100, anchor='w')
     tree2.place(x=30, y=55)
     tree2.bind('<<TreeviewSelect>>', item_selected2)
+
     cargar_tabla1(
         '', "SELECT id,NuHabitacion,Tipo,Costo,Dias,SubTotal,Descuento,Total FROM cliente WHERE Estado = 1 AND NuHabitacion LIKE")
     cargar_tabla2()
--- menu.py
+++ menu.py
@@ -4,26 +4,37 @@ from tkinter import ttk
 import cargarModificar
 import huespedes
 
+
 def ventana_Cargar_Modificar():
     menu.withdraw()
     cargarModificar.ventana_Carga_Modificacion(menu)
+
 
 def ventana_Huespedes():
     menu.withdraw()
     huespedes.ventana_huespedes(menu)
 
+
 menu = tk.Tk()
 forma = ttk.Frame(menu, padding=10)
 menu.config(bg="#000")
+menu.title("MENU")
+menu.resizable(0, 0)
+menu.protocol("WM_DELETE_WINDOW", menu)
 forma.grid()
 
-boton_Cargar_Huesped = ttk.Button(forma, text="Cargar Huesped", command=ventana_Huespedes)
-boton_Cargar_Huesped.grid(column=1, row=1, padx=20, pady=20, sticky="nsew", columnspan = 3)
+boton_Cargar_Huesped = ttk.Button(
+    forma, text="Cargar Huesped", command=ventana_Huespedes)
+boton_Cargar_Huesped.grid(column=1, row=1, padx=20,
+                          pady=20, sticky="nsew", columnspan=3)
 
-boton_Carga_Modificar = ttk.Button(forma, text="Cargar o Modificar Habitaciones", command=ventana_Cargar_Modificar)
-boton_Carga_Modificar.grid(column=1, row=2, padx=20, pady=20, sticky="nsew", columnspan = 3)
+boton_Carga_Modificar = ttk.Button(
+    forma, text="Cargar o Modificar Habitaciones", command=ventana_Cargar_Modificar)
+boton_Carga_Modificar.grid(column=1, row=2, padx=20,
+                           pady=20, sticky="nsew", columnspan=3)
 
 boton_Finalizar = ttk.Button(forma, text="Salir", command=menu.destroy)
-boton_Finalizar.grid(column=1, row=3, padx=20, pady=20, sticky="nsew", columnspan = 3)
+boton_Finalizar.grid(column=1, row=3, padx=20, pady=20,
+                     sticky="nsew", columnspan=3)
 
 menu.mainloop()
