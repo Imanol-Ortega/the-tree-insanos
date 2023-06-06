1a5ac5fb88bd1d0bb0072eaff5947b8d4e469fa7
EddieOnLive <eduardowelchenhein@gmail.com>
2023-06-06 18:46:33 -0400

    Menu update gráfica 2.1

--- menu.py
+++ menu.py
@@ -6,34 +6,34 @@ import cargarModificar
 import huespedes
 import formulario_Tp_C
 
-
+#?  Llama al archivo con la función de modificación y carga de habitaciones existentes
 def ventana_Cargar_Modificar():
     menu.withdraw()
     cargarModificar.ventana_Carga_Modificacion(menu)
 
-
+#?  Llama al archivo con la función de manejo de huéspedes
 def ventana_Huespedes():
     menu.withdraw()
     huespedes.ventana_huespedes(menu)
 
+#?  Llama al archivo con la función de manejo de ingresos por tipo de habitación
 def ventana_Formulario():
     menu.withdraw()
     formulario_Tp_C.ventana_Formulario_TP(menu)
 
+#?  Construyo y doy estilo a la ventana
 menu = tk.Tk()
-forma = ttk.Frame(menu, padding=10)
 menu.title("Menu")
+menu.geometry("230x300")
+menu.configure(background='#202123')
 
-estiloFreim = ttk.Style()
-estiloFreim.configure('fondo.TFrame', background='#202123')
-forma = ttk.Frame(menu, padding=10, style='fondo.TFrame')
-
-forma.grid()
+#?  Evita que se pueda redimensionar, maximizar y cerrar usando los botones propios de windows
 menu.resizable(0, 0)
 menu.protocol("WM_DELETE_WINDOW", menu)
 
+#?  Botón para ir a la función de manejo de huéspedes
 boton_Cargar_Huesped = ctk.CTkButton(
-    forma,
+    menu,
     text="Cargar Huesped",
     corner_radius=0,
     font=("Helvetica", 12, "bold"),
@@ -48,7 +48,8 @@ boton_Cargar_Huesped.grid(column=1,
                         sticky="nsew",
                         columnspan=3)
 
-boton_Carga_Modificar = ctk.CTkButton( forma,
+#?  Botón para ir a la función de manejo de habitaciones
+boton_Carga_Modificar = ctk.CTkButton( menu,
                                     text="Cargar o Modificar Habitaciones",
                                     corner_radius=0,
                                     font=("Helvetica", 12, "bold"),
@@ -63,7 +64,8 @@ boton_Carga_Modificar.grid( column=1,
                             sticky="nsew",
                             columnspan=3)
 
-boton_Formulario_Tp = ctk.CTkButton(   forma,
+#?  Botón para ir a la función de manejo de ingresos por tipo de habitación
+boton_Formulario_Tp = ctk.CTkButton(   menu,
                                     text="Ingresos por Habitacion",
                                     corner_radius=0,
                                     font=("Helvetica", 12, "bold"),
@@ -78,7 +80,8 @@ boton_Formulario_Tp.grid(   column=1,
                             sticky="nsew",
                             columnspan=3)
 
-boton_Finalizar = ctk.CTkButton(   forma,
+#?  Botón para salir
+boton_Finalizar = ctk.CTkButton(   menu,
                                 text="Salir",
                                 corner_radius=0,
                                 font=("Helvetica", 12, "bold"),
